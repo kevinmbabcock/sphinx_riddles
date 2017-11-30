432307dee5d10f92f912700dd7ea63fd51305d3b
Kevin Babcock <kevinmbabcock@icloud.com>
2017-11-30 11:26:43 -0800

    route all pages to correct location

---
+++ app.rb
@@ -0,0 +1,48 @@
+require('sinatra')
+require('sinatra/reloader')
+also_reload('lib/**/*.rb')
+require('./lib/riddles')
+require('pry')
+
+get('/') do
+  erb(:index)
+end
+
+get('/question1') do
+  erb(:question1)
+end
+
+post('/question1') do
+  answer = params.fetch("question1")
+  if (answer == "3")
+    erb(:question2)
+  else
+    erb(:fail)
+  end
+end
+
+# get('/question2') do
+#   erb(:question2)
+# end
+
+post('/question2') do
+  answer = params.fetch("question2")
+  if (answer == "ice cream")
+    erb(:question3)
+  else
+    erb(:fail)
+  end
+end
+
+# get('question3') do
+#   erb(:question3)
+# end
+
+post('/question3') do
+  answer = params.fetch("question3")
+  if (answer == "man")
+    erb(:success)
+  else
+    erb(:fail)
+  end
+end
